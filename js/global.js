77fd414e3ff68019f10f73fc130f7b69bfc56c63
Daniel Buchner <danieljb2@gmail.com>
2015-09-26 23:15:56 -0700

    draft update

---
+++ js/global.js
@@ -0,0 +1,5 @@
+
+
+xtag.addEvent(document.getElementById('register_handler'), 'tap', function(event){
+  navigator.webProfile.registerHandler('MS Profile Service', 'http://www.backalleycoder.com/web-profile/handler.html');
+});
